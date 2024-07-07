8b6a0212949f07865fb305921fa6b8717ef48cb0
C220174 <C220174@e.ntu.edu.sg>
2024-07-08 00:05:48 +0800

    t

---
+++ back_end/project/HelloWorld/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views  # . means impprt from ur project/app
+
+urlpatterns = [
+    path('hello/',views.Hello),   # '' means is the home page and the url is app level
+    path('page/', views.page),
+]
